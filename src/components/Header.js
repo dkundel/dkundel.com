0f65750ca0a98856b7db3f5bac5bbb74225bbbce
Dominik Kundel <dominik.kundel@gmail.com>
2018-03-17 23:54:03 +0100

    :bug: Make header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,44 @@ import { lightGray, secondaryGreen } from 'anker-colors';
 
 import Anchor from './Anchor';
 
+const headerBreakpoints = {
+  small: '515px',
+  tiny: '242px',
+};
+
 const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-between;
   background: ${lightGray};
   padding: 20px 20px;
   width: 100%;
+  min-height: fit-content;
+
+  @media (max-width: ${headerBreakpoints.small}) {
+    flex-direction: column;
+  }
 `;
 const Navigation = styled.nav`
+  display: flex;
+  flex-wrap: wrap;
   a {
     margin-left: 20px;
+  }
+
+  @media (max-width: ${headerBreakpoints.small}) {
+    margin-top: 10px;
+
+    a {
+      margin-left: 0;
+      margin-right: 20px;
+    }
   }
 `;
 
 const PageTitleContainer = styled.hgroup`
   margin: 0;
   display: flex;
+  flex-wrap: wrap;
 
   :hover {
     svg {
@@ -34,6 +56,12 @@ const PageTitleContainer = styled.hgroup`
     height: 1.5em;
     transform: rotateZ(22.5deg);
     transition: transform 0.5s ease;
+  }
+
+  @media (max-width: ${headerBreakpoints.tiny}) {
+    svg {
+      margin-bottom: 10px;
+    }
   }
 `;
 
