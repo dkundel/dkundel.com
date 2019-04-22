18c2f72b38916fa222edf936fb1bf23badbaa5d8
dkundel <dominik.kundel@gmail.com>
2019-04-21 17:30:13 -0700

    :bug: Fix build

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import styled from "styled-components"
 import Html from "../components/Html"
 import InfoHeader from "../components/InfoHeader"
 import Layout from "../components/layout"
-import SEO from "../components/Seo"
+import SEO from "../components/seo"
 
 const SocialList = styled.ul`
   list-style: none;
