a1f93a99864a0b321dcc540680215b1744c179b1
Dominik Kundel <hi@dominik.dev>
2022-11-20 22:24:48 -0800

    fix lighthouse

--- src/components/Html.jsx
+++ src/components/Html.jsx
@@ -24,7 +24,7 @@ export const allowImages = {
   allowedAttributes: {
     ...sanitize.defaults.allowedAttributes,
     '*': ['class'],
-    img: ['src', 'width', 'height'],
+    img: ['src', 'width', 'height', 'alt'],
   },
 };
 
