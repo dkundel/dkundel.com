69431a09037d00ff00aec8917a6c8c8fdd9d9bc9
Dominik Kundel <hi@dominik.dev>
2022-11-20 22:14:01 -0800

    remove redundant component

--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from 'typography';
-import noriegaTheme from 'typography-theme-noriega';
-
-const typography = new Typography(noriegaTheme);
-export const { rhythm, scale, options } = typography;
-
-export default typography;
