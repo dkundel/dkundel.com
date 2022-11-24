01e15340d39d15a128c681f64ea0f12940336d0d
Dominik Kundel <hi@dominik.dev>
2022-11-24 10:57:36 -0800

    feat: serve blog post headers locally

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,32 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { imagetools } from 'vite-imagetools';
 import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig( /** @type {import('astro').AstroUserConfig} */
-{
-  site: 'https://dkundel.com/',
-  vite: {
-    plugins: [imagetools(), VitePWA()]
-  },
-  integrations: [react(), tailwind({
-    config: {
-      path: './tailwind.config.cjs',
-      applyAstroPreset: false,
-      applyBaseStyles: false
-    }
-  }), sitemap()]
-});
+export default defineConfig(
+  /** @type {import('astro').AstroUserConfig} */
+  {
+    site: 'https://dkundel.com/',
+    vite: {
+      plugins: [imagetools(), VitePWA()],
+    },
+    integrations: [
+      react(),
+      tailwind({
+        config: {
+          path: './tailwind.config.cjs',
+          applyAstroPreset: false,
+          applyBaseStyles: false,
+        },
+      }),
+      sitemap(),
+      image(),
+    ],
+  }
+);
--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -4,18 +4,31 @@ const {
   updateRelativeLinksFromBase,
 } = require('about-json');
 const got = require('got');
-const { resolve } = require('path');
-const { writeFile: fsWriteFile } = require('fs');
+const { resolve, extname, relative } = require('path');
+const { writeFile: fsWriteFile, createWriteStream } = require('fs');
+const stream = require('stream');
+const crypto = require('crypto');
+const globLib = require('glob');
 const { promisify } = require('util');
 const cheerio = require('cheerio');
 
 const writeFile = promisify(fsWriteFile);
+const pipeline = promisify(stream.pipeline);
+const glob = promisify(globLib);
 
 const RAW_GITHUB = `https://raw.githubusercontent.com/dkundel/about-me`;
 const PAGE_GITHUB = `https://github.com/dkundel/about-me/blob`;
 const DATA_URL = `${RAW_GITHUB}/master/README.md`;
 const OUTPUT = resolve(process.cwd(), 'data/about/dkundel.json');
 const OUTPUT_MD = resolve(process.cwd(), 'data/dkundel-md.json');
+const OUTPUT_IMAGES = resolve(process.cwd(), 'public/blog-headers/external/');
+
+function getLocalImageFileName(baseUrl) {
+  return crypto
+    .createHash('sha256')
+    .update(baseUrl)
+    .digest('hex');
+}
 
 async function run() {
   const resp = await got(DATA_URL);
@@ -40,10 +53,31 @@ async function run() {
       };
       const url = entry.match(/href="(.+)"/)[1];
       out.url = url;
-      const resp = await got(url);
-      const $ = cheerio.load(resp.body);
-      const image = $('meta[property="og:image"]').attr('content');
-      out.image = image;
+      const localImageId = getLocalImageFileName(url);
+
+      const existingImageResults = await glob(
+        OUTPUT_IMAGES + '/' + localImageId + '.*'
+      );
+
+      if (existingImageResults.length < 1) {
+        const resp = await got(url);
+        const $ = cheerio.load(resp.body);
+        const image = $('meta[property="og:image"]').attr('content');
+        out.image = image;
+        const localImageFile = localImageId + (extname(image) || '.jpg');
+
+        const imageDownloadStream = got.stream(image);
+        const imageWriteStream = createWriteStream(
+          OUTPUT_IMAGES + '/' + localImageFile
+        );
+        await pipeline(imageDownloadStream, imageWriteStream);
+        out.image = '/blog-headers/external/' + localImageFile;
+      } else {
+        out.image =
+          '/blog-headers/external/' +
+          relative(OUTPUT_IMAGES, existingImageResults[0]);
+      }
+
       return out;
     })
   );
