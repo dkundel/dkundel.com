8663a400779db7af8034c781051dcb6623d9282a
Dominik Kundel <hi@dominik.dev>
2022-11-24 10:10:26 -0800

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,23 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { imagetools } from 'vite-imagetools';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig(
-  /** @type {import('astro').AstroUserConfig} */
-  {
-    site: 'https://dkundel.com/',
-    vite: {
-      plugins: [imagetools(), VitePWA()],
-    },
-    integrations: [
-      react(),
-      tailwind({
-        config: {
-          path: './tailwind.config.cjs',
-          applyAstroPreset: false,
-          applyBaseStyles: false,
-        },
-      }),
-    ],
-  }
-);
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig( /** @type {import('astro').AstroUserConfig} */
+{
+  site: 'https://dkundel.com/',
+  vite: {
+    plugins: [imagetools(), VitePWA()]
+  },
+  integrations: [react(), tailwind({
+    config: {
+      path: './tailwind.config.cjs',
+      applyAstroPreset: false,
+      applyBaseStyles: false
+    }
+  }), sitemap()]
+});
