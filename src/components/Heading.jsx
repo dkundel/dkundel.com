a660919021e2b16279cb18113ceca2e42794b637
Dominik Kundel <hi@dominik.dev>
2022-11-25 22:41:41 -0800

    fix: adjust more spacing issues

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,6 +3,6 @@ import styled from '../utils/styled';
 
 const Heading = styled('hgroup', 'min-w-[7rem] flex-1 text-sm');
 Heading.h1 = styled('h1', 'text-6xl large-headings font-black mb-2 text-highlight-gradient leading-[1.4em]');
-Heading.p = styled('p', 'mb-2 text-basee');
+Heading.p = styled('p', 'mb-2 text-base');
 
 export default Heading;
--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '../utils/styled';
 
-const SectionHeader = styled('header', 'flex justify-between items-start flex-wrap flex-row-reverse content-end');
+const SectionHeader = styled('header', 'flex justify-between items-start flex-wrap flex-row-reverse content-end mb-4');
 
 export default SectionHeader;
